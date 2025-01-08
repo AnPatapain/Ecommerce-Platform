bd4a5ae6d1f0e005158a2f6779d2cf5a2ef7b1f4
kean <nkalk192002@gmail.com>
2025-01-08 09:47:50 +0100

    Minor fix frontend config

--- packages/frontend/src/frontend-config.ts
+++ packages/frontend/src/frontend-config.ts
@@ -3,16 +3,8 @@
  * author: Ke An Nguyen
  */
 
-type EnvironmentConfig = {
-    NODE_ENV?: 'development' | 'production' | 'test',
-    PUBLIC_URL?: string,
-};
+const env: ImportMetaEnv = import.meta.env;
 
-const env: EnvironmentConfig = import.meta.env as any;
-
-export const CONFIG: typeof env= {
-    // Default value, these variables can be overwritten by ...env
-    NODE_ENV: 'development',
-    PUBLIC_URL: 'https://localhost',
-    ...env,
+export const CONFIG: any= {
+    PUBLIC_URL: env.MODE === 'production' ? 'https://vps-3131144f.vps.ovh.net' : 'https://localhost'
 }
